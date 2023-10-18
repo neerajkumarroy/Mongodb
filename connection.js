070f91ee1461c1944764860d8a7d232c7af30e95
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-10-18 14:12:40 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,17 +1,18 @@
-const {mongoClient} = require('mongodb');
-const url = "mongodb+srv://DEMO1:<DEMO1>@cluster0.1ufxrfa.mongodb.net/";
-const database = "college";
-const client = new mongodbClient(url);
+const {mongoClient} = require('mongodb');      // Import MongoClient from the 'mongodb' library.
+const url = "mongodb+srv://DEMO1:<DEMO1>@cluster0.1ufxrfa.mongodb.net/";   // Your MongoDB connection URL.
+const database = "college";             // Name of the database you want to connect to.
+
+const client = new MongodbClient(url);     // Create a new MongoClient instance.
 
 
 async function getData()
 {
-    let result = await client.connect();
-    let db = result.client(database);
-    let collection = db.collection('student');
-    let response = await collection.find({}).toArray();
-    console.log(response);
+    let result = await client.connect();           // Connect to the MongoDB database.
+    let db = result.client(database);              // Get a reference to the 'college' database.
+    let collection = db.collection('student');     // Get a reference to the 'student' collection within the 'college' database.
+    let response = await collection.find({}).toArray();    // Retrieve all documents in the 'student' collection and convert them to an array.
+    console.log(response);          // Print the result to the console.
 
 }
 
-getData();
+getData();   // Call the getData function to retrieve data from MongoDB.  
