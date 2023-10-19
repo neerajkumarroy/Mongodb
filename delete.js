efd177a7181acabd4d401097991af02dc9c0c257
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-10-19 11:48:24 +0530

    Update delete.js

--- delete.js
+++ delete.js
@@ -2,7 +2,7 @@ const dbConnect = require("./config");
 
 const deleteData = async() => {
     let data = await dbConnect();
-    let result = data.deleteOne({name:"note 8"})
+    let result = await data.deleteOne({name:"note 8"})
     console.warn(result);
     if(result.acknowledged)
     {
