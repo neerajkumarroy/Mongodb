65b7e132272eb425694fac71219dbfc6ed94ac48
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-11-27 12:08:19 +0530

    Import_Export

---
+++ oper.js
@@ -0,0 +1,11 @@
+//1== const oadd = require("./index");
+//2== const oper = require("./index");
+const {add,sub,mul} = require("./index");  //--3
+// console.log(oper);
+
+
+// 2== console.log(oper.sub(10,5));
+
+console.log(add(5,5)); // all are mandatory
+console.log(sub(10,5));
+console.log(mul(10,5));
