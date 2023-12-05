12a7fc690b9dc7f300b36e0e8d09551651efe9ef
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-12-05 11:58:49 +0530

    Create_multiple_file

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
-const http = require('http');
-const fs = require('fs');
-const express = require("express");
-const app = express();
+const fs = require("fs");
+const path = require("path");
 
+const dirpath = path.join(__dirname,'FILES');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    
-    // Create a writable stream to a file named 'output.txt'
-    const writeStream = fs.createWriteStream('output.txt');
-    
-    // Write data to the stream
-    app.get("/",(req,resp) => {
-        console.log(writeStream)
-    
+//Create multiple file with the help of command line.
+
+for(let i=1;i<6;i++)
+{
+
+    fs.writeFileSync(dirpath+"/hello"+i+".txt", "This is my first file");
+
+}
+
+//Read all created file
+fs.readdir(dirpath,(err,files)=>{
+    files.forEach((item)=>{
+        console.log("My file name is:"+item);
     })
-  writeStream.write('Hello, My name is Neeraj Kumar');
-  writeStream.write('And Now I am working in MNC companny as a Nodejs Developer');
-  writeStream.end(); // Close the stream
-  
+})
 
-  // Event listener for the 'finish' event, triggered when the write stream is finished
-  writeStream.on('finish', () => {
-    console.log('Data has been written to output.txt');
-  });
-
-  // Event listener for the 'error' event, triggered if an error occurs during writing
-  writeStream.on('error', (err) => {
-    console.error('Error writing to file:', err);
-  });
-
-  res.end('Data written to file. Check the console for details.\n');
-});
-
-const PORT = 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
